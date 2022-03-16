5db7ac6003fad52ace3bf07140f40feaaf2b511f
jmarc-25 <jrodrigues16mf@gmail.com>
2022-03-16 13:09:28 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("João", "Azul", 12);
+		System.out.println(gato);
+		
+		
+		/*int a = 5;
+		int b = 3;
+		System.out.println("Hello World " + (a+b));*/
+
+	}
+
+}
